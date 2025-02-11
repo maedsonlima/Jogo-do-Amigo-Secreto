e74cca19e56a6f21da19035827b88c6d7f4af9c1
maedson lima <maedsonrhcp@gmail.com>
2025-02-11 01:57:40 -0300

    Adicionar nomes digitados na lista

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-//teste
+let ListaDeAmigos = [];
+
+
+function adicionarAmigo() {
+ListaDeAmigos.push(document.getElementById('amigo').value);
+
+console.log (ListaDeAmigos);
+}
